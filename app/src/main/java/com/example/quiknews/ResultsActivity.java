d9a27c26140b4b7b92858a48b06937506608e69e
Jake Maro <maro2@illinois.edu>
2019-12-02 17:16:52 -0600

    first commit

--- app/src/main/java/com/example/quiknews/ResultsActivity.java
+++ app/src/main/java/com/example/quiknews/ResultsActivity.java
@@ -3,6 +3,8 @@ package com.example.quiknews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResultsActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class ResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("This button closes this activity and launches the SearchActivity.");
+            }
+        });
+
+        //launching this activity sends a GET request with the search keyword from the intent and returns a response that contains the articles related to the keyword
+        //then, the linear layout called article list is inflated with the articleChunk.
     }
 }
--- app/src/main/java/com/example/quiknews/SearchActivity.java
+++ app/src/main/java/com/example/quiknews/SearchActivity.java
@@ -3,6 +3,9 @@ package com.example.quiknews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        Button searchButton = findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("this button simply creates an intent that contains the string of the text in the search bar and launches ResultsActivity");
+            }
+        });
+
+        // when launching the results activity, create an intent that lets the user access the
     }
 }
